529cc6ef286bb4543030d7d734e9218ceca4e29e
sahil <gautamsahil1947>
2023-05-09 00:02:01 +0530

    first commit

---
+++ lua/josean/plugins/harpoon.lua
@@ -0,0 +1,18 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>m", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "t", function()
+  ui.nav_file(1)
+end)
+vim.keymap.set("n", "n", function()
+  ui.nav_file(2)
+end)
+vim.keymap.set("n", "q", function()
+  ui.nav_file(3)
+end)
+vim.keymap.set("n", "<C-s>", function()
+  ui.nav_file(4)
+end)
